f23284a1fa90aa996c512092d5f74b8b57d66500
MizukiShigihara <mizuki.shigihara@zozo.com>
2025-02-07 09:16:29 +0900

    feat: オンライン判定用のredis処理を追加

---
+++ internal/infrastructure/redis/user_presence.go
@@ -0,0 +1,43 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+const keyOnlinePrefix = "online:"
+
+type UserPresence struct {
+	Client *Client
+}
+
+func NewUserPresence(client *Client) *UserPresence {
+	return &UserPresence{Client: client}
+}
+
+func (up *UserPresence) SetOnline(ctx context.Context, userID string) error {
+	key := fmt.Sprintf(keyOnlinePrefix + "%s", userID)
+	return up.Client.Set(ctx, key, true, 30*time.Second).Err()
+}
+
+func (up *UserPresence) IsOnline(ctx context.Context, userID string) (bool, error) {
+	key := fmt.Sprintf(keyOnlinePrefix + "%s", userID)
+	result, err := up.Client.Exists(ctx, key).Result()
+	if err != nil {
+		return false, err
+	}
+	return result == 1, nil
+}
+
+func (up *UserPresence) GetOnlineUsers(ctx context.Context) ([]string, error) {
+	keys, err := up.Client.Keys(ctx, keyOnlinePrefix + "*").Result()
+	if err != nil {
+		return nil, err
+	}
+	users := make([]string, 0, len(keys))
+	for _, key := range keys {
+		users = append(users, key[len(keyOnlinePrefix):])
+	}
+	return users, nil
+}
